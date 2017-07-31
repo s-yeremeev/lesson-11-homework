9633bbaa05ef0e63fe9859f283dee3d97b4e775f
Sasha Yeremeev <c3pions@gmail.com>
2017-07-31 23:46:43 +0300

    2

--- src/game.js
+++ src/game.js
@@ -1,55 +1,70 @@
 class Game extends Square {
-    constructor(){
-        super(color)
+    constructor(...props) {
+        super(...props)
 
-        this.checkSquare = function(idsquare, event) {
-                    const blueRect = document.getElementById("blueRect");
-                    // получаем стиль для blueRect
-                    const cs = window.getComputedStyle(blueRect);
-                    
-                    var left = parseInt(cs.marginLeft);
-                    var top = parseInt(cs.marginTop);
-                    
-                    switch (event.keyCode){                        
-                        case 37:  // если нажата клавиша влево
-                            if(left>0)
-                                
-                            break;
-                        case 38:   // если нажата клавиша вверх
-                            if(top>0)
-                               
-                            break;
-                        case 39:   // если нажата клавиша вправо
+        function getColor(tag) {
+            // var toHex = function(color) {
+            //     var hex = function(str) {
+            //     let result = parseInt(str).toString(16);
+            //     if (result.length < 2)
+            //         result = '0' + result;
+            //     return result;
+            //     }
+            // let rgb = color.match(/^rgb\(\s*(\d+),\s*(\d+),\s*(\d+)\s*\)$/);
+            // if (!rgb)
+            // return color;
+            // return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+            // }
+        let style = window.getComputedStyle(tag);
+        return style.backgroundColor;
+        }
+
+         this.checkSquare = function() {
+             const slides = document.getElementsByClassName("slides")
+             let ind2 = ind
+             if(ind2 >= slides.length) ind2 = 0
+             let color = getColor(document.getElementsByClassName("slides")[ind2])
                             
-                            break;
-                        case 40:   // если нажата клавиша вниз
-                            
-                            break;
-                            }
-                        }
- 
-                            addEventListener("keydown", moveRect);
-    }
+                            console.log(color)         
+                                 
+                            // switch (event.keyCode){                        
+                            //     case 37:  // если нажата клавиша влево
+                            //         if(left>0)
+                                        
+                            //         break;
+                            //     case 38:   // если нажата клавиша вверх
+                            //         if(top>0)
+                                    
+                            //         break;
+                            //     case 39:   // если нажата клавиша вправо
+                                    
+                            //         break;
+                            //     case 40:   // если нажата клавиша вниз
+                                    
+                            //         break;
+                            //         }
+                            //     }
+        
+                            //         addEventListener("keydown", moveRect);
+     }
+}
 }
 
-switch(event.keyCode) {
-    case "":{
-        break
-    }
-    case "":{
-        break
-    }
-    case "":{
-        break
-    }
-    case "":{
-        break
-    }
-    case "":{
-        break
-    }
-     default:{
-    
-    break
-    }
+
+function Timer(ms) {
+    this.ms = ms
+    this.interval = null
 }
+
+
+Timer.prototype.start = function (callback) {
+    this.interval = setInterval(callback, this.ms)
+    return this
+}
+
+const game = new Game()
+const timer = new Timer(1000)
+timer.start(function () {
+  game.visibleSquare() 
+  game.checkSquare()
+    })
--- src/square.js
+++ src/square.js
@@ -1,62 +1,20 @@
+var ind = 0
 class Square {
-    constructor(color) {
-    this.color = color
+    constructor() {
 
-    this.canvasAdd = function () {
-      const canvas = document.getElementById("newSquare");  
-      if (canvas.getContext) {
-      const ctx = canvas.getContext('2d');
-      ctx.fillStyle = color;
-      ctx.fillRect(25,25,100,100);
+    this.visibleSquare = function () {
+                          let i = 0
+                          let slides = document.getElementsByClassName("slides")
+                          for (i = 0; i < slides.length; i++) {
+                              slides[i].style.display = "none"; 
+                          }
+                          ind++;
+                          if (ind > slides.length) {
+                             ind = 1
+                          } 
+                          slides[ind-1].style.display = "block"; 
       }
-    }
     }
 }
 
-// function Timer(ms) {
-//     this.ms = ms
-//     this.interval = null
-// }
-
-// /**
-//  * function for start count in ms
-//  */
-// Timer.prototype.start = function (callback) {
-//     this.interval = setInterval(callback, this.ms)
-//     return this
-// }
-
-// /**
-//  * function for clear interval
-//  */
-// Timer.prototype.stop = function () {
-//     clearInterval(this.interval)
-//     return this
-// }
-
-// /**
-//  * function for stop count in ms
-//  */
-// Timer.prototype.stopAfter = function (ms, callback) {
-//     setTimeout(callback, ms)
-//     return this
-// }
-
-// const sq = new Square("red")
-//     sq.canvasAdd()
-
-// const timer = new Timer(1000)
-// timer.start(function () {
-//      if(color == "red"){
-//          let col = "blue"   
-//          console.log(col) 
-//      } else {
-//          let col = "red"
-//          console.log(col) 
-//      }
-//         const sq = new Square(col)
-//         console.log(col) 
-//         sq.canvasAdd()
-    
-//           })
-//   .stopAfter(15000, timer.stop.bind(timer))
+const square = new Square()
